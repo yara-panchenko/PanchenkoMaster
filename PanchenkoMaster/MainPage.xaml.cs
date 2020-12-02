90851b0dc1abfed4fe5b1ae33a1f5c938fd9c2dc
Opilane TTHK <opilane@tthk.ee>
2020-12-02 11:01:28 +0200

    Add project files.

---
+++ PanchenkoMaster/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PanchenkoMaster
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            profileImage.Source = ImageSource.FromFile("4.jpg");
+            aboutList.ItemsSource = GetMenuList();
+            var homePage = typeof(Views.AboutPage);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+
+        }
+        public List<MasterMenuItems> GetMenuList()
+        {
+            var list = new List<MasterMenuItems>();
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minust",
+                Detail = "Lühike info",
+                ImagePath = "info.jpg",
+                TargetPage = typeof(Views.AboutPage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu kogemus",
+                Detail = "Natuke rohkem minu kogemisest",
+                ImagePath = "menu.png",
+                TargetPage = typeof(Views.ExperiencePage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu oskused",
+                Detail = "Natuke rohkem minu oskustest",
+                ImagePath = "oskused.jpg",
+                TargetPage = typeof(Views.SkillsPage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu võidud",
+                Detail = "Ma olen uhke!!!",
+                ImagePath = "voidud.png",
+                TargetPage = typeof(Views.Achievements)
+            });
+        }
+
+        private void aboutList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenuItem = (MasterMenuItems)e.SelectedItem;
+            Type selectedPage = selectedMenuItem.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+            IsPresented = false;
+        }
+    }
+}
---
+++ PanchenkoMaster/MasterMenuItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanchenkoMaster
+{
+    public class MasterMenuItems
+    {
+        public string Text { get; set; }
+        public string Detail { get; set; }
+        public string ImagePath { get; set; }
+        public Type TargetPage { get; set; }
+
+    }
+}
